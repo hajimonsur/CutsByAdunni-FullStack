21727b9cb850b6f65bb5c90196a924d6c6dbf120
hajimonsur <taiwomonsuru29@gmail.com>
2025-01-27 11:23:33 +0100

    sqqq

--- cutByAdunni-Backend/index.js
+++ cutByAdunni-Backend/index.js
@@ -28,5 +28,5 @@ app.use("/api/contact", require("./routes/contacts"));
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
